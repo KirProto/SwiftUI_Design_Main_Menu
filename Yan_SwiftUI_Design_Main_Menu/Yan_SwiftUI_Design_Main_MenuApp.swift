e8b26ec7a3854df73add438fcc798d0699e2fc25
Kir Proto Q <kir.proto@ya.ru>
2022-02-17 14:48:23 +0500

    Initial Commit

---
+++ Yan_SwiftUI_Design_Main_Menu/Yan_SwiftUI_Design_Main_MenuApp.swift
@@ -0,0 +1,17 @@
+//
+//  Yan_SwiftUI_Design_Main_MenuApp.swift
+//  Yan_SwiftUI_Design_Main_Menu
+//
+//  Created by KIR Q on 2022.02.17.
+//
+
+import SwiftUI
+
+@main
+struct Yan_SwiftUI_Design_Main_MenuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
